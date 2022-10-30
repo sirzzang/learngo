5e4188a93294c1866cee5f15848c0f119458b981
sirzzang <sirzzang@naver.com>
2022-10-30 23:10:32 +0900

    feat: add basic goroutine

--- main.go
+++ main.go
@@ -1,51 +1,19 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("request failed")
-
 func main() {
-
-	var results map[string]string = make(map[string]string)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://www.github.com/",
-		"https://academy.nomadcoders.co/",
-	}
-
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAIL"
-		}
-		results[url] = result
-	}
-
-	for url, result := range results {
-		fmt.Println(url, result)
-	}
-
+	go coolCount("sirzzang")
+	go coolCount("eraser")
+	time.Sleep(time.Second * 5)
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(url, ":", err, resp.StatusCode)
-		return errRequestFailed
+func coolCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is cool", i)
+		time.Sleep(time.Second)
 	}
-	return nil
 }
