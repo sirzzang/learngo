6934bf3c3b05356884ea4bf7b5835719dcfebae2
sirzzang <sirzzang@naver.com>
2022-10-30 23:52:38 +0900

    feat: add gochannel

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	go coolCount("sirzzang")
-	go coolCount("eraser")
-	time.Sleep(time.Second * 5)
+
+	c := make(chan string)
+
+	people := [5]string{"sirzzang", "eraser", "ieere", "ruby", "sir"}
+	for _, person := range people {
+		go isCool(person, c) // two goroutines
+	}
+	for i := 0; i < len(people); i++ {
+		fmt.Println("received: " + <-c)
+	}
 }
 
-func coolCount(person string) {
-	for i := 0; i < 10; i++ {
-		fmt.Println(person, "is cool", i)
-		time.Sleep(time.Second)
-	}
+func isCool(person string, c chan string) {
+	c <- person + " is cool" // send true value to channel
+	fmt.Println("sending message")
 }
