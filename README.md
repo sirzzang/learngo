2d819e7292c08794eded318e96689f3df401e415
sirzzang <sirzzang@naver.com>
2022-11-03 22:00:27 +0900

    feat: add goquery

---
+++ README.md
@@ -0,0 +1,17 @@
+# Job Scraper Built With Go
+
+## install goquery
+- [goquery](https://github.com/PuerkitoBio/goquery)
+```bash
+$ go get github.com/PuerkitoBio/goquery
+```
+```bash
+go: downloading github.com/PuerkitoBio/goquery v1.8.0
+go: downloading github.com/andybalholm/cascadia v1.3.1
+go: downloading golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
+go: added github.com/PuerkitoBio/goquery v1.8.0
+go: added github.com/andybalholm/cascadia v1.3.1
+go: added golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
+```
+
+
--- main.go
+++ main.go
@@ -2,53 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-type requestResult struct {
-	url    string
-	status string
-}
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
 func main() {
-
-	results := make(map[string]string)
-	c := make(chan requestResult)
-
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-		"https://www.github.com/",
-	}
-
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-
-	for url, status := range results {
-		fmt.Println(url, status)
-	}
-
+	pages := getPages()
+	fmt.Println(pages)
 }
 
-func hitURL(url string, c chan<- requestResult /** send only channel **/) {
-	fmt.Println("Checking: " + url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		c <- requestResult{url: url, status: "FAIL"}
-	} else {
-		c <- requestResult{url: url, status: "OK"}
+func getPages() int {
+	resp, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(resp)
+
+	defer resp.Body.Close() // prevent memory leak
+
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	checkErr(err)
+
+	fmt.Println(doc)
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(resp *http.Response) {
+	if resp.StatusCode != 200 {
+		log.Fatalln("Request failed with status:", resp.StatusCode)
 	}
 }
